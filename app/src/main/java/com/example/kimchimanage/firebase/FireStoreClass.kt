43e7a1354dbb6273b3921db4e1746b2c1d3c0d62
Yiro Yi <yiroyi@live.com>
2022-09-17 23:15:55 +0900

    Added SignIn Data

--- app/src/main/java/com/example/kimchimanage/firebase/FireStoreClass.kt
+++ app/src/main/java/com/example/kimchimanage/firebase/FireStoreClass.kt
@@ -1,5 +1,7 @@
 package com.example.kimchimanage.firebase
 
+import android.util.Log
+import com.example.kimchimanage.activities.SignInActivity
 import com.example.kimchimanage.activities.SignUpActivity
 import com.example.kimchimanage.models.User
 import com.example.kimchimanage.utils.Constants
@@ -17,6 +19,19 @@ class FireStoreClass {
       .set(userInfo, SetOptions.merge())
       .addOnSuccessListener {
         activity.userRegisteredSuccess()
+      }.addOnFailureListener {
+        e ->
+        Log.e(activity.javaClass.simpleName, "Error")
+      }
+  }
+
+  fun signInUser(activity: SignInActivity) {
+    mFireStore.collection(Constants.USERS)
+      .document(getCurrentId())
+      .get()
+      .addOnSuccessListener { document ->
+        val loggedInUser = document.toObject(User::class.java)!!
+        activity.signInSuccess(loggedInUser)
       }.addOnFailureListener {
         e ->
         Log.e(activity.javaClass.simpleName, "Error")
