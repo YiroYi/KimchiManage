e25365e634dcfa54e11f64531ee186d2aa3c7bd3
Yiro Yi <yiroyi@live.com>
2022-09-19 18:02:09 +0900

    Uploading Image to FireStore

--- app/src/main/java/com/example/kimchimanage/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/kimchimanage/activities/MyProfileActivity.kt
@@ -8,7 +8,9 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.WindowManager
+import android.webkit.MimeTypeMap
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -16,6 +18,9 @@ import com.bumptech.glide.Glide
 import com.example.kimchimanage.R
 import com.example.kimchimanage.firebase.FireStoreClass
 import com.example.kimchimanage.models.User
+import com.google.api.ResourceProto.resource
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_my_profile.*
 import java.io.IOException
 
@@ -27,6 +32,7 @@ class MyProfileActivity : BaseActivity() {
   }
 
   private var mSelectedImageFileUri: Uri? = null
+  private var mProfileImageURL: String? = ""
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -50,6 +56,12 @@ class MyProfileActivity : BaseActivity() {
           arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
           READ_STORAGE_PERMISSION_CODE
         )
+      }
+    }
+
+    btn_update.setOnClickListener {
+      if(mSelectedImageFileUri != null) {
+        uploadUserImage()
       }
     }
   }
@@ -122,5 +134,46 @@ class MyProfileActivity : BaseActivity() {
     if(user.mobile != 0L) {
       et_mobile.setText(user.mobile.toString())
     }
+  }
+
+  private fun uploadUserImage() {
+    showProgressDialog(resources.getString(R.string.please_wait))
+
+    if (mSelectedImageFileUri != null) {
+      val sRef : StorageReference =
+        FirebaseStorage.getInstance().reference.child("USER_IMAGE" + System.currentTimeMillis() + getFileExtension(mSelectedImageFileUri))
+
+      sRef.putFile(mSelectedImageFileUri!!).addOnSuccessListener {
+        taskSnapshot ->
+        Log.i(
+          "Firebase Image URL",
+          taskSnapshot.metadata!!.reference!!.downloadUrl.toString()
+        )
+
+        taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener {
+          uri ->
+          Log.i("Downloadable Image URL", uri.toString())
+          mProfileImageURL = uri.toString()
+
+          hideProgressDialog()
+
+        }
+      }.addOnFailureListener{
+        exception ->
+        Toast.makeText(
+        this@MyProfileActivity,
+        exception.message,
+        Toast.LENGTH_LONG
+        ).show()
+
+        hideProgressDialog()
+      }
+    }
+  }
+
+  private fun getFileExtension(uri: Uri?): String? {
+    return MimeTypeMap
+      .getSingleton()
+      .getExtensionFromMimeType(contentResolver.getType(uri!!))
   }
 }
