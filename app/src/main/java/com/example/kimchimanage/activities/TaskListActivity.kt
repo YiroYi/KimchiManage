7f6f1f185c691cfaad5d8de391c6bbe19c6d17e0
Yiro Yi <yiroyi@live.com>
2022-10-02 00:52:52 +0900

    Added Adapter for TaskList

--- app/src/main/java/com/example/kimchimanage/activities/TaskListActivity.kt
+++ app/src/main/java/com/example/kimchimanage/activities/TaskListActivity.kt
@@ -3,9 +3,12 @@ package com.example.kimchimanage.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kimchimanage.R
+import com.example.kimchimanage.adapters.TaskListItemsAdapter
 import com.example.kimchimanage.firebase.FireStoreClass
 import com.example.kimchimanage.models.Board
+import com.example.kimchimanage.models.Task
 import com.example.kimchimanage.utils.Constants
 import kotlinx.android.synthetic.main.activity_task_list.*
 
@@ -44,5 +47,19 @@ class TaskListActivity : BaseActivity() {
   fun boardDetails(board: Board) {
     hideProgressDialog()
     setupActionBar(board.name)
+
+    val addTaskList = Task(resources.getString(R.string.add_list))
+    board.taskList.add(addTaskList)
+
+    rv_task_list.layoutManager = LinearLayoutManager(
+      this,
+      LinearLayoutManager.HORIZONTAL,
+      false
+    )
+
+    rv_task_list.setHasFixedSize(true)
+
+    val adapter = TaskListItemsAdapter(this, board.taskList)
+    rv_task_list.adapter = adapter
   }
 }
