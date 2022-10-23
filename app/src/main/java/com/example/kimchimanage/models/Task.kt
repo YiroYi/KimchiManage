e70a0f049da579b8fc3c18095ea487d3f565beae
Yiro Yi <yiroyi@live.com>
2022-10-23 17:59:32 +0900

    Added card to list

--- app/src/main/java/com/example/kimchimanage/models/Task.kt
+++ app/src/main/java/com/example/kimchimanage/models/Task.kt
@@ -2,14 +2,17 @@ package com.example.kimchimanage.models
 
 import android.os.Parcel
 import android.os.Parcelable
+import com.projemanag.model.Card
 
 data class Task (
   var title: String = "",
   var createdBy: String = "",
+  val cards: ArrayList<Card> = ArrayList()
   ): Parcelable {
   constructor(parcel: Parcel) : this(
     parcel.readString()!!,
-    parcel.readString()!!
+    parcel.readString()!!,
+    parcel.createTypedArrayList(Card.CREATOR)!!
   )
 
   override fun describeContents() = 0
@@ -17,6 +20,7 @@ data class Task (
   override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
     writeString(title)
     writeString(createdBy)
+    writeTypedList(cards)
   }
 
   companion object CREATOR : Parcelable.Creator<Task> {
